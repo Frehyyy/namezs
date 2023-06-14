1a38f5d86e25de51998e31005b2f1e522597e220
Frehyyy <89162555+Frehyyy@users.noreply.github.com>
2023-06-14 19:42:24 +0500

    Delete NameZS-main directory

--- NameZS-main/README.md
+++
@@ -1,2 +0,0 @@
-# Demo version [here](https://namezs-full.vercel.app/ "NameZS — Demo Version")
-#### notice: The server in the demo can periodically work out
--- NameZS-main/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AboutPage from '@components/About/index.jsx'
-const About = () => {
-    return <AboutPage/>;
-};
-
-export default About;
